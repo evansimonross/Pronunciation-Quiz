33abc506440fc36ae042f22e624e5e654288c391
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-09 12:15:53 -0400

    Added report of incorrect choices, with audio replayability.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ var quiz = {
     correctAnswers: 0,
     incorrectAnswers: 0,
     timesUp: 0,
+    toReport: [],
     nextQuestion: function () {
         $('#choices').empty();
         $('#timer').text(quiz.timeLimit);
@@ -23,13 +24,14 @@ var quiz = {
         var choices = quiz.questionPool[choicesIndex];
         quiz.questionPool.splice(choicesIndex, 1);
         var correctIndex = Math.floor(Math.random() * choices.length);
+        var correctWord = choices[correctIndex];
 
         // Print the correct answer to the log. Delete this for final deployment.
-        console.log('The correct answer is ' + choices[correctIndex]);
+        console.log('The correct answer is ' + correctWord);
 
         // Set up audio file for the correct answer
         var audio = $('audio');
-        audio.attr('src', "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/" + choices[correctIndex] + "_us_1.mp3");
+        audio.attr('src', "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/" + correctWord + "_us_1.mp3");
         audio[0].addEventListener('loadedmetadata', function () {
             audio[0].play();
         });
@@ -66,6 +68,8 @@ var quiz = {
                     $('#incorrect').text(quiz.incorrectAnswers);
                     clearInterval(countdownInterval);
                     clearTimeout(countdownTimeout);
+                    var youSaid = $(this).text();
+                    quiz.toReport.push({correctWord, youSaid});
                     quiz.nextQuestion();
                 });
             }
@@ -93,9 +97,38 @@ var quiz = {
         },1000*(quiz.timeLimit+1));
 
     },
-    displayReport: function () {
 
+    // Report the results of the quiz.
+    displayReport: function () {
+        for(var i=0; i<quiz.toReport.length;i++){
+            $('#choices').append('<div id="report' + i + '"></div>');
+            var youSaidButton = $('<button type="button" id="highFront" class="btn btn-danger btn-lg reportButton">' + quiz.toReport[i].youSaid + '</button>');
+            youSaidButton.on('click',function(){
+                var audio = $('audio');
+                audio.attr('src', "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/" + $(this).text() + "_us_1.mp3");
+                audio[0].addEventListener('loadedmetadata', function () {
+                    audio[0].play();
+                });
+                var newAudio = audio[0].cloneNode(true);
+                audio[0].parentNode.replaceChild(newAudio, audio[0]);
+            })
+            var correctButton = $('<button type="button" id="highFront" class="btn btn-success btn-lg reportButton">' + quiz.toReport[i].correctWord + '</button>');
+            correctButton.on('click',function(){
+                var audio = $('audio');
+                audio.attr('src', "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/" + $(this).text() + "_us_1.mp3");
+                audio[0].addEventListener('loadedmetadata', function () {
+                    audio[0].play();
+                });
+                var newAudio = audio[0].cloneNode(true);
+                audio[0].parentNode.replaceChild(newAudio, audio[0]);
+            })
+            $('#report' + i).append(youSaidButton);
+            $('#report' + i).append(correctButton);
+        }
+        $('#choices').append('<button type="button" id="highFront" class="btn btn-primary btn-lg answerButton">Try again</button>');
     },
+
+    // Check whether all the audio files in the current question pool are accessable. 
     checkAudioFiles: function() {
         for(var i=0; i<quiz.questionPool.length;i++){
             var questionSet = quiz.questionPool[i];
@@ -103,7 +136,7 @@ var quiz = {
                 var audio = $('audio');
                 audio.attr('src', "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/" + questionSet[j] + "_us_1.mp3");
                 audio[0].addEventListener('loadedmetadata', function () {
-                    //audio[0].play();
+                    // An error will be thrown to the console if the audio file does not exist and the metadata cannot load.
                 });
                 var newAudio = audio[0].cloneNode(true);
                 audio[0].parentNode.replaceChild(newAudio, audio[0]);
@@ -112,6 +145,7 @@ var quiz = {
     }
 }
 
+// Sets click listeners for the options buttons at the start
 $(document).ready(function () {
     $('#highFront').on('click', function(){
         quiz.questionPool = highFrontVowels;
@@ -119,6 +153,10 @@ $(document).ready(function () {
     $('#highBack').on('click', function(){
         quiz.questionPool = highBackVowels;
     });
+
+    // Set this as our final function to check if there are any erroneous file names.
     //$('.answerButton').on('click',quiz.checkAudioFiles);
+
+    // Set this as our final function for main quiz functionality.
     $('.answerButton').on('click',quiz.nextQuestion);
 });
